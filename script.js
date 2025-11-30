18c1537b3dc2b8b568ca18364317e54c7a260cf6
Sachini <msachini959@gmail.com>
2025-11-30 13:20:01 +0530

    Updated script file

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const navLinks=document.querySelectorAll('.header nav a');
+const logoLink=document.querySelectorAll('.logo');
+
+const activePage = () => {
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+}
+
+navLinks.forEach((link,idx) => {
+    link.addEventListener('click', () => {
+           if(!link.classList.contains('active')) {
+                activePage();
+
+                link.classList.add('active');
+           }
+    });
+});
+
+logoLink.addEventListener('click',() => {
+    if(!navLinks[0].classList.contains('active')){
+        activePage();
+
+        navLinks[0].classList.add('active');
+    }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const resumeBtns = document.querySelectorAll('.resume-btn');
   const resumeDetails = document.querySelectorAll('.resume-detail');
